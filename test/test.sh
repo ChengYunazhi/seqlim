3f41f4e0eee4085747147dfe0065d8c9bfe1f010
kyungtaekLIM <shallowest.sea@gmail.com>
2016-12-14 17:58:01 +0900

    edit tests

--- test/test.sh
+++ test/test.sh
@@ -1,19 +1,19 @@
-python ../seqlim.py cath ./samples/fasta/
-python ../seqlim.py cath ./samples/fasta/ -outfmt fasta
-python ../seqlim.py cath ./samples/fasta/ -outfmt nex
-python ../seqlim.py cath ./samples/fasta/ -outfmt phylip
-python ../seqlim.py cath ./samples/fasta/ -outfmt tsv
-python ../seqlim.py cath ./samples/fasta/ -outfmt nex -o ./samples/new_locus1.nex 
-python ../seqlim.py cath ./samples/fasta/ -outfmt phylip -o ./samples/new_locus1.phy
-python ../seqlim.py cath ./samples/fasta/ -outfmt phylip -line_length 50 -block_length 10
-python ../seqlim.py catv ./samples/fasta/
-python ../seqlim.py catv ./samples/fasta/ -outfmt fasta
-python ../seqlim.py catv ./samples/fasta/ -outfmt nex
-python ../seqlim.py catv ./samples/fasta/ -outfmt phylip
-python ../seqlim.py catv ./samples/fasta/ -outfmt nex -o ./samples/new_locus1-v.nex 
-python ../seqlim.py catv ./samples/fasta/ -outfmt phylip -o ./samples/new_locus1-v.phy
-python ../seqlim.py cnvt ./samples/fasta/ -outfmt nex -o ./temp
-python ../seqlim.py cnvt ./samples/fasta/ -outfmt phylip -o ./temp
-python ../seqlim.py cnvt ./samples/fasta/ -outfmt csv -o ./temp
+python ../seqlim.py cath ./fasta/
+python ../seqlim.py cath ./fasta/ -outfmt fasta
+python ../seqlim.py cath ./fasta/ -outfmt nex
+python ../seqlim.py cath ./fasta/ -outfmt phylip
+python ../seqlim.py cath ./fasta/ -outfmt tsv
+python ../seqlim.py cath ./fasta/ -outfmt nex -o ./temp/new_locus1.nex 
+python ../seqlim.py cath ./fasta/ -outfmt phylip -o ./temp/new_locus1.phy
+python ../seqlim.py cath ./fasta/ -outfmt phylip -line_length 50 -block_length 10
+python ../seqlim.py catv ./fasta/
+python ../seqlim.py catv ./fasta/ -outfmt fasta
+python ../seqlim.py catv ./fasta/ -outfmt nex
+python ../seqlim.py catv ./fasta/ -outfmt phylip
+python ../seqlim.py catv ./fasta/ -outfmt nex -o ./temp/new_locus1-v.nex 
+python ../seqlim.py catv ./fasta/ -outfmt phylip -o ./temp/new_locus1-v.phy
+python ../seqlim.py cnvt ./fasta/ -outfmt nex -o ./temp
+python ../seqlim.py cnvt ./fasta/ -outfmt phylip -o ./temp
+python ../seqlim.py cnvt ./fasta/ -outfmt csv -o ./temp
 python ../seqlim.py cnvt ./samples/test.phylip -infmt phylip -outfmt fasta
 python ../seqlim.py cnvt ./samples/test.phylip -infmt phylip -outfmt fasta -remove -
